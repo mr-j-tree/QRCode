fae8dba6a0f50037f3fd82ae14e3938f8fb086a6
joelstewart <joel@codeinversion.com>
2019-10-07 09:17:45 -0500

    Move import around

--- Sources/QRCode/UIImageViewExtension.swift
+++ Sources/QRCode/UIImageViewExtension.swift
@@ -6,13 +6,9 @@
 //  Copyright (c) 2015 Alexander Schuch. All rights reserved.
 //
 
-#if canImport(AppKit)
-import AppKit
-#else
-import UIKit
-#endif
-
 #if canImport(UIKit)
+
+import UIKit
 
 public extension UIImageView {
     
